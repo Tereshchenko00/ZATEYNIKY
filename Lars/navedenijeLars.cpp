0a53a84d05a551c514e8db9adde3911805f650c6
Larsik <arsenijlizunkov@gmail.com>
2025-12-28 16:26:48 +0300

    add sample1

---
+++ Lars/navedenijeLars.cpp
@@ -0,0 +1,34 @@
+struct Mirror{
+	public:
+		int angleX = 0;
+		int angleY = 0;
+
+		void turnX(int angle){
+			///
+			return 0;
+		}
+
+		void turnY(int angle){
+			///
+			return 0;
+		}
+		void getPos(){
+			return angleX, angleY;
+		}
+}
+
+
+
+
+void setup(){
+
+
+}
+
+void loop(){
+
+
+
+}
+
+
